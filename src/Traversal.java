460289b1981ea8d1fca112b57b401eda03c56a42
Akaioka <brendanvillaraza@yahoo.com>
2026-02-19 14:30:51 -0800

    Completed livecode

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Traversal {
   public static void main(String[] args) {
     TreeNode<Integer> root = new TreeNode<Integer>(10, null, null);
@@ -10,13 +13,13 @@ public class Traversal {
     root.right.right = new TreeNode<Integer>(5, null, null);
     root.right.right.right = new TreeNode<Integer>(22, null, null);
 
-    TreeNode<String> stringRoot = new TreeNode<String>("Hello", null, null);
+    TreeNode<String> stringRoot = new TreeNode<String>("hello", null, null);
     stringRoot.left = new TreeNode<String>("bine", null, null);
-    stringRoot.left.left = new TreeNode<String>("time", null, null);
+    stringRoot.left.left = new TreeNode<String>("hello", null, null);
     stringRoot.left.right = new TreeNode<String>("lime", null, null);
 
     stringRoot.right = new TreeNode<String>("chime", null, null);
-    stringRoot.right.left = new TreeNode<String>("bind", null, null);
+    stringRoot.right.left = new TreeNode<String>("lime", null, null);
     stringRoot.right.right = new TreeNode<String>("climb", null, null);
     stringRoot.right.right.right = new TreeNode<String>("help", null, null);
     System.out.println();
@@ -32,8 +35,24 @@ public class Traversal {
 
     // inOrder(stringRoot);
     // printGreater(root, 4);
-    System.out.println(countNodes(stringRoot));
+    // System.out.println(countNodes(stringRoot));
+    System.out.println(toMap(stringRoot));
   }
+  public static <T> Map<T, Integer> toMap(TreeNode<T> node){
+    Map<T, Integer> counts = new HashMap<>();
+    toMap(node, counts);
+    return counts;
+  }
+
+  private static <T> void toMap(TreeNode<T> node, Map<T, Integer> counts){
+    if(node == null) { return;}
+    // fill up those counts
+    counts.put(node.value, counts.getOrDefault(node.value, 0) + 1);
+    toMap(node.left, counts);
+    toMap(node.right, counts);
+  }
+
+  
   public static int countNodes(TreeNode<?> node){
     return node == null ? 0 : 1 + countNodes(node.left) + countNodes(node.right);
     // if (node == null) { return 0; }
